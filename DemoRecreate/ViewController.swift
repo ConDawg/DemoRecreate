fb7e0f0c77c3bd02a86227244498649d6f923332
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-10-10 12:16:58 -0600

    Recrated the app we previously made in class

--- DemoRecreate/ViewController.swift
+++ DemoRecreate/ViewController.swift
@@ -8,14 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var firstButton: UIButton!
+    
+    @IBAction func firstMethod(_ sender: Any)
+    {
+        if( firstButton.backgroundColor == .orange )
+        {
+            firstButton.backgroundColor = .black
+        }
+        else
+        {
+            firstButton.backgroundColor = .orange
+        }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let greenValue :CGFloat = CGFloat (Double(arc4random_uniform(256)) / 255.00)
+        let blueValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
